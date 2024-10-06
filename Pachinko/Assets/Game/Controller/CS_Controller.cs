1e0bdaa33a17edadf4fe0e6b848a69125fb4073b
nakashima <aiyinzhongdao@gmail.com>
2024-10-06 18:50:40 +0900

    保留玉と演出が連動して動くように統合

--- Pachinko/Assets/Game/Controller/CS_Controller.cs
+++ Pachinko/Assets/Game/Controller/CS_Controller.cs
@@ -23,10 +23,21 @@ public class CS_Controller : MonoBehaviour
     [SerializeField, Header("司令塔コントローラー")]
     List<GameObject> mCtrls = new List<GameObject>();
 
+    [SerializeField, Header("ヘソ")]
+    private Heso mHeso;
+
+    [SerializeField, Header("図柄表示")]
+    private CS_DrawPattern mDrawNum;
+
     private PACHINKO_PHESE mNowPhese = PACHINKO_PHESE.SET;//現在のフェーズ
     private PACHINKO_PHESE mPrevPhese = PACHINKO_PHESE.SET;//前ののフェーズ
 
     private int mStock = 0;//保留玉数
+    
+    private bool mPatternVariationFinish = true;//図柄変動終了フラグ
+    private bool mPerformanceFinish = true;//演出終了フラグ
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -39,8 +50,6 @@ public class CS_Controller : MonoBehaviour
     {
         //現在のフェーズの前のフェーズが違うなら次のフェーズに行く
         if(mNowPhese != mPrevPhese) { GoNextPhese(); }
-
-       
     }
 
     //現在のフェーズ取得
@@ -72,12 +81,38 @@ public class CS_Controller : MonoBehaviour
 
     public void UseStock()
     {
-        mStock--;
+       // mStock--;
+        mPatternVariationFinish = false;//図柄変動終了フラグをfalse
+        mPerformanceFinish = false;//演出終了フラグをfalse
+
+        //Debug.Log("図柄:" + mHeso.stock[0][0] + "," + mHeso.stock[0][1] + "," + mHeso.stock[0][2] + ",");
+        mDrawNum.StartPatternVariation();//
     }
+
+    
 
     //保留玉を取得する
     public int GetStock()
     {
+        mStock = mHeso.stock.Count;
         return mStock;
+    }
+
+    //演出終了
+    public void PerformanceFinish()
+    {
+        mPerformanceFinish = true;
+    }
+
+    //図柄変動終了
+    public void PatternVariationFinish()
+    {
+        mPatternVariationFinish = true;
+    }
+
+    //変動が開始できるか
+    public bool CanVariationStart()
+    {
+        return mPatternVariationFinish && mPerformanceFinish;
     }
 }
